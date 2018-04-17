4a87222008bb2cee963ed05bfd208a0e32edaafd
danielChoque <patzidaniel@gmail.com>
2018-04-17 10:19:53 -0400

    con ip

--- src/app/model/Network.ts
+++ src/app/model/Network.ts
@@ -1,5 +1,6 @@
 export class Network {
     public static API_URL = "http://192.168.100.17:8080/";
+    //public static API_URL = "http://192.168.100.42:8080/";
     //public static API_URL = "http://backendyingul-env.cqx28e6j2j.us-west-2.elasticbeanstalk.com/";
     public static API_KEY = "RUREWVFVRU5BTExBVEE6ZWRkeQ==";
     public static BUCKET_URL ="https://s3-us-west-2.amazonaws.com/jsa-s3-bucketimage/dev/image/";
