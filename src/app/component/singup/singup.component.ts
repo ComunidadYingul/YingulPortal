749f528792ddd7527375310e5d62c18cd5faef5d
gonzalo <gmeruviarojas@gmail.com>
2018-04-25 11:19:00 -0400

    CORRECCION Y VALIDADO DEL FORMULARIO DE REGISTRO (SIGNUP)

--- src/app/component/singup/singup.component.ts
+++ src/app/component/singup/singup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ElementRef} from '@angular/core';
 import {Observable}  from 'rxjs/Observable';
 import { SingupService } from '../../service/singup.service';
 import { Router } from '@angular/router';
@@ -28,11 +28,15 @@ export class SingupComponent implements OnInit {
   hidName:boolean=true;
   hidLastname:boolean=true;
   hidEmail:boolean=true;
+  hidEmail2:boolean=true;
+  popup_registro:boolean=true;
+  popup_exist:boolean=true;
+  popup_terminos:boolean=true;
   hidPassword:boolean=true;
   popup_g:boolean=true;
   cbxPersona:boolean;
 
-  constructor(private singupService: SingupService, private router: Router,private sellService: SellService) {   
+  constructor(private elem:ElementRef,private singupService: SingupService, private router: Router,private sellService: SellService) {   
     
   }
   ngOnInit() {
@@ -46,16 +50,21 @@ export class SingupComponent implements OnInit {
     var patron = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
     if(this.name==null || this.name==""){
         this.hidName=false;
+        this.elem.nativeElement.querySelector('#name').focus();
       }else if(this.lastname==null || this.lastname==""){
         this.hidLastname=false;
+        this.elem.nativeElement.querySelector('#lastname').focus();
       }else if(this.email==null || this.email==""){
         this.hidEmail=false;
+        this.elem.nativeElement.querySelector('#email').focus();
       }else if(this.password==null || this.password==""){
         this.hidPassword=false;
+        this.elem.nativeElement.querySelector('#password').focus();
       }else if(this.cbxPersona!=true){
-        alert('Debe aceptar los terminos y condiciones');
+        this.popup_terminos=false;
       }else if(!patron.test(this.email)){
-        alert('Corre electronico no valido');
+        this.hidEmail2=false;
+        this.elem.nativeElement.querySelector('#email').focus();
       }else{
         this.popup_g=false;
 
@@ -64,12 +73,16 @@ export class SingupComponent implements OnInit {
           res => {
                 this.msg = JSON.parse(JSON.stringify(res))._body;
                 if(this.msg=='save'){
-                  alert("registrado exitosamente revise su bandeja de entrada");
+                  this.popup_registro=false;
                   this.router.navigate(['/']);   
                 }
                 else{
                   this.popup_g=true;
-                  alert(this.msg);
+                  if(this.msg=='email exist'){
+                    this.popup_exist=false;
+                  }else{
+                    alert(this.msg);
+                  }
                 } 
               },
               error => {
@@ -80,10 +93,16 @@ export class SingupComponent implements OnInit {
       }
   }
 
+  popupHide(){
+    this.popup_exist=true;
+    this.popup_terminos=true;
+  }
+
   reset(){
     this.hidName=true;
     this.hidLastname=true;
     this.hidEmail=true;
+    this.hidEmail2=true;
     this.hidPassword=true;
   }
 
