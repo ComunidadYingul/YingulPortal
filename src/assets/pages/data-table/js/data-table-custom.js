92d81ee0c8f2eb7551be0f8bd2c472589a2e1834
gonzalo <gmeruviarojas@gmail.com>
2018-05-03 18:27:53 -0400

    CORRECCION DE LAS VISTAS TIPO FACEBOOK

--- src/assets/pages/data-table/js/data-table-custom.js
+++ src/assets/pages/data-table/js/data-table-custom.js
@@ -507,11 +507,12 @@ $(document).ready(function() {
         "language": {
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ registros",
-            "sZeroRecords":    "No se encontraron resultados",
+            /*"sZeroRecords":    "No se encontraron resultados",*/
+            "sZeroRecords":    "",
             /*"sEmptyTable":     "Ningún dato disponible en esta tabla",*/
             "sInfo":           "Mostrando _END_ registros de un total de _TOTAL_",
             /*"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",*/
-            "sInfoEmpty":      "Mostrando 0 registros de un total de 0",
+            "sInfoEmpty":      "",
             "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
             "sInfoPostFix":    "",
             "sSearch":         "Buscar:",
@@ -535,12 +536,13 @@ $(document).ready(function() {
         "language": {
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ registros",
-            "sZeroRecords":    "No se encontraron resultados",
+            /*"sZeroRecords":    "No se encontraron resultados",*/
+            "sZeroRecords":    "",
             /*"sEmptyTable":     "Ningún dato disponible en esta tabla",*/
             /*"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",*/
             "sInfo":           "Mostrando _END_ registros de un total de _TOTAL_",
             /*"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",*/
-            "sInfoEmpty":      "Mostrando 0 registros de un total de 0",
+            "sInfoEmpty":      "",
             "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
             "sInfoPostFix":    "",
             "sSearch":         "Buscar:",
@@ -567,12 +569,13 @@ $(document).ready(function() {
         "language": {
             "sProcessing":     "Procesando...",
             "sLengthMenu":     "Mostrar _MENU_ registros",
-            "sZeroRecords":    "No se encontraron resultados",
+            /*"sZeroRecords":    "No se encontraron resultados",*/
+            "sZeroRecords":    "",
             /*"sEmptyTable":     "Ningún dato disponible en esta tabla",*/
             /*"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",*/
             "sInfo":           "Mostrando _END_ registros de un total de _TOTAL_",
             /*"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",*/
-            "sInfoEmpty":      "Mostrando 0 registros de un total de 0",
+            "sInfoEmpty":      "",
             "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
             "sInfoPostFix":    "",
             "sSearch":         "Buscar:",
