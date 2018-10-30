35fb4345ad9134727267c1e56866c738b1b5963f
Eddy <quenallataeddy@gmail.com>
2018-10-30 14:56:38 -0400

    sin direcciones

--- src/app/model/Network.ts
+++ src/app/model/Network.ts
@@ -1,16 +1,15 @@
 export class Network {
     //public static API_URL = "http://192.168.100.16:8080/";
     //public static API_URL = "http://192.168.100.17:8080/";
-    //public static API_URL="http://localhost:8080/Yingul-0.0.2-SNAPSHOT/";
     //public static API_URL = "http://192.168.100.60:8080/";
     //public static API_URL = "http://192.168.100.51:8080/";
     //public static API_URL = "http://192.168.0.14:8080/";
     //public static API_URL = "http://192.168.100.51:8080/";
-    public static API_URL = "http://localhost:8080/";
-    //public static API_URL = "https://api.yingul.com/";   
+    //public static API_URL = "http://localhost:8080/";
+    public static API_URL = "https://api.yingul.com/";   
     public static API_KEY = "RUREWVFVRU5BTExBVEE6ZWRkeQ==";
     //public static BUCKET_URL ="https://s3-us-west-2.amazonaws.com/jsa-s3-bucketimage/dev/image/";
     public static BUCKET_URL ="https://s3-us-west-2.amazonaws.com/jsa-s3-bucketimage/image/";
-    //public static PROTOCOL_URL = "https://";
-    public static PROTOCOL_URL = "http://";
+    public static PROTOCOL_URL = "https://";
+    //public static PROTOCOL_URL = "http://";
 }
